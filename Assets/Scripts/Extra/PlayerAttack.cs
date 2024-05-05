aaeee2c3d8bce7251a262c7ebcb9c20a2632a4dc
Bruno Grou <61094081+bgrou@users.noreply.github.com>
2024-05-05 22:56:59 +0100

    sync

---
+++ Assets/Scripts/Extra/PlayerAttack.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    public GameObject AttackSphere;
+    public Transform ShootingPoint;
+    public float ShootingForce = 1000f;
+    public float ShootingTime = 0.5f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ShootProjectile();
+        }
+    }
+
+    void ShootProjectile()
+    {
+        if (AttackSphere && ShootingPoint)
+        {
+
+            GameObject my_projetile = Instantiate(AttackSphere, ShootingPoint.position, ShootingPoint.rotation);
+            Rigidbody2D my_body = my_projetile.GetComponent<Rigidbody2D>();
+            if (my_body)
+            {
+                Debug.LogError(ShootingPoint.right);
+                my_body.AddForce(ShootingPoint.right * ShootingForce);
+            }
+            else
+            {
+                Debug.LogError("NOT WORKING");
+            }
+            Destroy(my_projetile, ShootingTime);
+
+        }
+    }
+
+
+}
